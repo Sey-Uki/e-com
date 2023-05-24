eae651f55593f174a3902ba074e9157b7b769f74
Mavludin <imevlud@gmail.com>
2023-05-24 18:22:04 +0300

    feat(product): add details tab

---
+++ src/components/product/tabs/details.tsx
@@ -0,0 +1,21 @@
+import s from './tabs.module.css';
+import { MainTitle } from '../../mainTitle/mainTitle';
+import { Price } from '../../price/price';
+
+type Props = {
+  price?: string;
+  title?: string;
+  description?: string;
+};
+
+export const DetailsTab = ({ price, title, description }: Props) => {
+  return (
+    <div className={s.details}>
+      <div className={s.price}>
+        <strong>Стоимость:</strong> <Price>{price}</Price>
+      </div>
+      <MainTitle>{title}</MainTitle>
+      <p>{description}</p>
+    </div>
+  );
+};
--- src/utils/products.ts
+++ src/utils/products.ts
@@ -2,21 +2,27 @@ export const PRODUCTS = [
   {
     id: 1,
     name: 'Hauteville Concrete Rocking Chair',
-    price: '100$',
+    price: '100₽',
+    description:
+      'Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.',
     thumbnail:
       'https://www.free-css.com/assets/files/free-css-templates/preview/page225/shop/assets/images/product-1.jpg',
   },
   {
     id: 2,
     name: 'Pavilion Speaker',
-    price: '150$',
+    price: '150₽',
+    description:
+      'Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.',
     thumbnail:
       'https://www.free-css.com/assets/files/free-css-templates/preview/page225/shop/assets/images/product-2.jpg',
   },
   {
     id: 3,
     name: 'LIGOMANCER',
-    price: '50$',
+    price: '50₽',
+    description:
+      'Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.',
     thumbnail:
       'https://www.free-css.com/assets/files/free-css-templates/preview/page225/shop/assets/images/product-3.jpg',
   },
