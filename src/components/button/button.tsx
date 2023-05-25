d4b00d1956606c03349b301936e88f048a6071d4
Mavludin <imevlud@gmail.com>
2023-05-25 14:55:39 +0300

    feat(product): addToCart button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,8 +2,13 @@ import s from './button.module.css';
 
 type Props = {
   children: React.ReactNode;
+  extraClass?: string;
 };
 
-export const Button = ({ children }: Props) => {
-  return <button className={s.button}>{children}</button>;
+export const Button = ({ children, extraClass }: Props) => {
+  return (
+    <button className={`${s.button} ${extraClass ? extraClass : ''}`}>
+      {children}
+    </button>
+  );
 };
