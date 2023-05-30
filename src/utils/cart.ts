d37053cd82519b1daf6690005ff20cfcffcb2c62
Mavludin <imevlud@gmail.com>
2023-05-30 14:25:36 +0300

    feat: add cart page

---
+++ src/utils/cart.ts
@@ -0,0 +1,16 @@
+export const CART_ITEMS = [
+  {
+    id: 1,
+    name: 'Кронштейн Желоба (MIN)',
+    price: '38₽',
+    amount: 1,
+    thumbnail: require('../../public/assets/images/brackets/bracket1.jpeg'),
+  },
+  {
+    id: 2,
+    name: 'Кронштейн Желоба (MAX)',
+    price: '42₽',
+    amount: 2,
+    thumbnail: require('../../public/assets/images/brackets/bracket2.webp'),
+  },
+];
